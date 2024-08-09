9816c4cae07008134eba92f0bd8884a221656a17
Syed Haris shah <haris.official222@gmail.com>
2024-08-09 18:55:46 +0500

    food menu style completed

--- src/components/FoodMenuList.jsx
+++ src/components/FoodMenuList.jsx
@@ -6,12 +6,12 @@ import FoodMenu from './FoodMenu'
 
 function FoodMenuList() {
     const foodNames = [
-        {name: 'pizza', image: pizza, price:'1000', ingredients:'Chiken and Mashrooms'},
-        {name: 'burger', image: burger, price:'500', ingredients:'Chiken and Cheese'},
-        {name: 'momos', image: momos, price:'100', ingredients:'Meet and Vegetables'},
-        {name: 'pizza', image: pizza, price:'1000', ingredients:'Chiken and Mashrooms'},
-        {name: 'burger', image: burger, price:'500', ingredients:'Chiken and Cheese'},
-        {name: 'momos', image: momos, price:'100', ingredients:'Meet and Vegetables'}
+        {name: 'Pizza', image: pizza, price:'1000', ingredients:'Chiken and Mashrooms'},
+        {name: 'Burger', image: burger, price:'500', ingredients:'Chiken and Cheese'},
+        {name: 'Momos', image: momos, price:'100', ingredients:'Meet and Vegetables'},
+        {name: 'Pizza', image: pizza, price:'1000', ingredients:'Chiken and Mashrooms'},
+        {name: 'Burger', image: burger, price:'500', ingredients:'Chiken and Cheese'},
+        {name: 'Momos', image: momos, price:'100', ingredients:'Meet and Vegetables'}
         
     ]
   return (
