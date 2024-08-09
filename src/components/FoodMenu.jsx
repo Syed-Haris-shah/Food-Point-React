495d46239638a5e3a132e05b04c6ada2ab657581
Syed Haris shah <haris.official222@gmail.com>
2024-08-09 21:15:28 +0500

    deploy food website on github

--- src/components/FoodMenu.jsx
+++ src/components/FoodMenu.jsx
@@ -6,7 +6,6 @@ function FoodMenu({foodNames}) {
     <div className='food-menu-container'>
         <h1 className='food-heading'>Food Menu</h1>
         <div className='food-menu-list'>
-
             {foodNames.map((food, index)=>(
                 <FoodCard key={index}
                 title={food.name}
