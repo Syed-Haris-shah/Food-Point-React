68ef1fc4c32218f959ef8424de7e8b75ed3fce76
Syed Haris shah <haris.official222@gmail.com>
2024-08-09 20:45:11 +0500

    footer section styling completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
   return (
     <div className='footer-container'>
       <div className='footer-img'>
-        <a href="https://www.facebook.com"><img src={Facebook} alt="" /></a>
-        <a href="https://www.instagram.com"><img src={Instagram} alt="" /></a>
-        <a href="https://www.twitter.com"><img className='twitter' src={Twitter} alt="" /></a>
+        <a href="https://www.facebook.com"><img src={Facebook} alt="facebook" /></a>
+        <a href="https://www.instagram.com"><img src={Instagram} alt="instagram" /></a>
+        <a href="https://www.twitter.com"><img className='twitter' src={Twitter} alt="twitter"/></a>
       </div>
       <p>Copyright &copy; 2024 All rights researved</p>
     </div>
